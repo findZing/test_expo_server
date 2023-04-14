43e4e8433b64eac6d0ad537cb26ee335ffd06010
Manh <manhnguyenhuu1001@gmail.com>
2023-04-14 10:47:02 +0700

    Change

---
+++ test_expo-av/client/App.js
@@ -0,0 +1,107 @@
+import { Audio } from "expo-av";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import axios from "axios";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import RecordingScreen from "./screen/RecordingScreen";
+import HandleFileScreen from "./screen/HandleFileScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as Updates from 'expo-updates';
+
+const urlServer = 'https://9553-27-76-236-105.ngrok-free.app'
+
+const Tab = createBottomTabNavigator()
+export default function App() {
+  const [sound, setSound] = useState();
+  const [recording, setRecording] = useState()
+  const [progress, setProgress] = useState(0)
+
+  const runTypeMessage = Updates.isEmbeddedLaunch
+  ? 'This app is running from built-in code'
+  : 'This app is running an update';
+  console.log(runTypeMessage)
+  const [recordings, setRecordings] = useState([]);
+
+  
+  
+
+  // const getRecordingLines = () => {
+  //   return recordings.map((recordingLine, index) => {
+  //     return (
+  //       <View key={index} style={styles.row}>
+  //         <Text style={styles.fill}>
+  //           Recording {index + 1} - {recordingLine.duration}
+  //         </Text>
+  //         <Button
+  //           title="Trim"
+  //           style={styles.button}
+  //           onPress={async () => {
+  //             recordingLine.sound.replayAsync();
+  //           }}
+  //         />
+  //       </View>
+  //     );
+  //   });
+  // };
+
+  // useEffect(() => {
+    
+  // }, [])
+
+  // useEffect(() => {
+  //   return sound
+  //     ? () => {
+  //         console.log("Unloading Sound");
+  //         sound.unloadAsync();
+  //       }
+  //     : undefined;
+  // }, [sound]);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Recording" screenOptions={{
+          headerShown: false,
+          tabBarIconStyle: {display: 'none'},
+          tabBarItemStyle: {
+            paddingVertical: 10
+          },
+          tabBarLabelStyle: {
+            fontSize: 20,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }
+          
+        }}
+        
+        >
+          <Tab.Screen name="Recording" component={RecordingScreen} options={{tabBarIconStyle: {display: 'none'}}}/>
+          <Tab.Screen name="Handle" component={HandleFileScreen}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    // display: "flex",
+    // flexDirection: "column",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // gap: 10,
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  fill: {
+    flex: 1,
+  },
+});
---
+++ test_expo-av/server/index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const db = require("./src/models");
+
+const app = express();
+app.use(fileUpload());
+
+app.use(cors());
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
+app.post('/upload', async (req, res) => {
+    if (!req.files || !req.files.audio) {
+      return res.status(400).send('No audio file uploaded');
+    }
+    
+    const audioFile = req.files.audio;
+    const fileName = `${Date.now()}-${audioFile.name}`;
+    
+    // audioFile.mv(path.join(__dirname, 'uploads', fileName), (err) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return res.status(500).send('Error uploading file');
+    //   }
+    //   res.send('File uploaded successfully');
+    // });
+
+    try {
+      const newAudioFile = await db.Audio.create({
+        name: fileName,
+        audio: audioFile.data
+      })
+
+      return res.status(200).json('File uploaded successfully');
+    } catch (error) {
+      return res.status(500).send('Error uploading file');
+    }
+  });
+
+  app.post('/audio', async (req, res) => {
+    try {
+      const audioFile = await db.Audio.findOne(
+        {
+          where: {
+            name: req.body.name+'.mp3',
+          },
+        }
+      )
+        // console.log(audioFile)
+      return res.status(200).json({
+        data: audioFile,
+        msg: 'Get all audio successful'
+      })
+    } catch (error) {
+      return res.status(404).json("Error get files")
+    }
+  });
+
+  app.get('/allaudio', async (req, res) => {
+    try {
+      const allAudio = await db.Audio.findAll(
+        {
+          raw: true
+        }
+      )
+      
+      return res.status(200).json({
+        data: allAudio,
+        msg: 'Get all audio successful'
+      })
+    } catch (error) {
+      return res.status(404).json("Error get files")
+    }
+  });
+app.get("/", (req, res) => {
+  res.status(200).json("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
